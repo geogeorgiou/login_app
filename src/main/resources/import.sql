72ffab68aea8193f809819d780161f5e320947c8
GeoGeorgiou <georgiogeo27@gmail.com>
2020-01-14 20:16:17 +0200

    [master] Providing sample import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,14 @@
+
+
+
+INSERT INTO LOGIN_USER (firstname,lastname,email,company,phoneNumber,password) VALUES ('Leo', 'Tolstoy','tolstoy@test.com','kresnis 4','2105443120','pass1234');
+INSERT INTO LOGIN_USER (firstname,lastname,email,company,phoneNumber,password) VALUES ('John', 'Steinbeck','stein@test.com','palalama 10','2104546481','pass1234');
+INSERT INTO LOGIN_USER (firstname,lastname,email,company,phoneNumber,password) VALUES ('Alexandros', 'Papadiamantis','pap@test.com','papanikoli 2','2103454191','pass1234');
+INSERT INTO LOGIN_USER (firstname,lastname,email,company,phoneNumber,password) VALUES ('Nikos', 'Kazantzakis','kazatz@test.com','karea 50','2102365678','pass1234');
+INSERT INTO LOGIN_USER (firstname,lastname,email,company,phoneNumber,password) VALUES ('Angelos', 'Terzakis','angterz@test.com','giannou 6','2107687321','pass1234');
+
+
+                                 
+
+
+
