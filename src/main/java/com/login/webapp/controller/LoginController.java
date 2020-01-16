59650f19b589eb522e4a79663938c9f003a5777e
GeoGeorgiou <georgiogeo27@gmail.com>
2020-01-16 13:42:34 +0200

    [master] Compressing jQuery code in compact form + POST mapping at LoginController

--- src/main/java/com/login/webapp/controller/LoginController.java
+++ src/main/java/com/login/webapp/controller/LoginController.java
@@ -4,6 +4,7 @@ package com.login.webapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 //controller to listen to "/login"
 
@@ -13,7 +14,13 @@ public class LoginController {
 
     //further implementation pending for spring security
     @GetMapping({"/login"})
-    public String login(Model model){
+    public String getLogin(Model model){
+        return "pages/login";
+    }
+
+    //post mapping to return after user POST register data
+    @PostMapping({"/login"})
+    public String postLogin(Model model){
         return "pages/login";
     }
 
--- src/main/resources/static/validator.js
+++ src/main/resources/static/validator.js
@@ -1,9 +1,9 @@
 jQuery(function ($){
 
-    const userRegister = $('#userRegister');
+    const userValidator = $('#userValidator');
 
-    if (userRegister.validate) {
-        userRegister.validate({
+    if (userValidator.validate) {
+        userValidator.validate({
             rules: {
 
                 firstName: {
@@ -21,12 +21,12 @@ jQuery(function ($){
                     required: true
                 },
 
+
                 confirm_password:{
                     required: true
                 },
 
                 phoneNumber: {
-                    // required: true,
                     digits: true,
                     minlength: 10,
                     maxlength: 10
@@ -61,57 +61,5 @@ jQuery(function ($){
         });
     }
 
-
-    const userHome = $('#userHome');
-    if (userHome.validate) {
-        userHome.validate({
-            rules: {
-
-                firstName: {
-                    required: true
-                },
-                lastName: {
-                    required: true
-                },
-                email: {
-                    required: true,
-                    email: true
-                },
-
-                password:{
-                    required: true
-                },
-
-                phoneNumber: {
-                    digits: true,
-                    minlength: 10,
-                    maxlength: 10
-                },
-
-            },
-            messages: {
-
-                email: {
-                    required: 'Enter your email! 📧',
-                },
-                firstName: {
-                    required: 'Enter your first name'
-                },
-                lastName: {
-                    required: 'Enter your last name'
-                },
-                password: {
-                    required: 'Enter your password'
-                },
-
-                phoneNumber: {
-                    minlength: 'Phone number should be 10 digits',
-                    maxlength: 'Phone number should be 10 digits',
-                    digits: 'Phone number should contain only digits'
-                },
-
-            }
-        });
-    }
 
 });
