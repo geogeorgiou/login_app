c7711b26c54d00b683fd62db7f9494eda67932a9
GeoGeorgiou <georgiogeo27@gmail.com>
2020-01-16 17:49:05 +0200

    [master] Added functionality to prevent form submission with invalid password

--- src/main/resources/static/passValidator.js
+++ src/main/resources/static/passValidator.js
@@ -1,5 +1,6 @@
 
-$(document).ready(function(){
+//function to call when submitting changes to password
+function passValidator() {
 
     //we define a regular expression to accept
     //at least 1 char [a-z]
@@ -7,20 +8,17 @@ $(document).ready(function(){
     //at least 1 number [0-9]
     //at least 1 special char
 
-    // var $regexname=/^( (?=.*[a-z]) | (?=.*[A-Z]) ) (?=.*[!@#$%^&*]) (?=.*[0-9]) [0-9a-zA-Z!@#$%^&*] {8,} $/;
-
     var $regexname = /^((?=.*[a-z])|(?=.*[A-Z]))(?=.*[!@#$%^&*])(?=.*[0-9])[0-9a-zA-Z!@#$%^&*]{8,}$/;
 
-    $('#password').on('keypress keydown keyup',function(){
-        if (!$(this).val().match($regexname)) {
-            // there is a mismatch, hence show the error message
-            $('.emsg').removeClass('hidden');
-            $('.emsg').show();
-        }
-        else{
-            // else, do not display message
-            $('.emsg').addClass('hidden');
-        }
-    });
-});
+    var password = document.getElementById("password").value;
 
+    //check if password matches regular expression format
+    if ($regexname.test(password)){
+        $('.emsg').addClass('hidden');
+        return true;
+    }
+    $('.emsg').removeClass('hidden');
+    $('.emsg').show();
+    return false;
+
+}
